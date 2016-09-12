9cf3b5d672ab4d1c9a4cd615251c277e77066827
Austin Morrell <austinlm19@gmail.com>
2016-09-12 11:07:12 -0500

    OpenGL set up

--- Rendering Graphics/Rendering Graphics/Main.cpp
+++ Rendering Graphics/Rendering Graphics/Main.cpp
@@ -1,6 +1,14 @@
-
+#include "src/gl_core_4_4.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+#define GLM_SWIZZLE
+#define GLM_FORCE_PURE
+#include "src\Gizmos.h"
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 int main()
 {
 
+	return 0;
 }
